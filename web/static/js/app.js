0ef877568c6b45a2e7402f6e39110fdf00bbcda5
Ricardo <bigardone@gmail.com>
2015-12-05 11:05:29 +0100

    Added video player js and styles

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -11,7 +11,7 @@
 //
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
-import "deps/phoenix_html/web/static/js/phoenix_html"
+import 'deps/phoenix_html/web/static/js/phoenix_html'
 
 // Import local files
 //
@@ -19,3 +19,11 @@ import "deps/phoenix_html/web/static/js/phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+import Player from './player'
+
+let video = document.getElementById('video')
+
+if (video) {
+  Player.init(video.id, video.getAttribute('data-player-id'))
+}
